00fc0ef11b82357f7b4ad1bdfa878d9b3318ca31
nancystchen <nanstchen@gmail.com>
2021-12-01 17:24:20 -0800

    prob 2 solution

--- 1/src/main.rs
+++ 1/src/main.rs
@@ -16,10 +16,46 @@ fn count_increased_depth(lines: Lines<BufReader<File>>) -> u32 {
     count
 }
 
+fn count_increased_sliding_window(lines: Lines<BufReader<File>>) -> u32 {
+    // first add up first 3
+    // then remove first, add last, compare
+    // keep doing it and put on counter
+    // when finish last available number, stop
+    let mut count = 0;
+    let mut sum = 0;
+    let mut first = 0;
+    let mut second = 0;
+    lines.enumerate().for_each(|(idx, line)| {
+        if let Ok(depth_string) = line {
+            let val = depth_string.parse::<u32>().unwrap();
+            if idx == 0 {
+                first = val;
+            } else if idx == 1 {
+                second = val;
+            } else if idx == 2 {
+                sum = first + second + val;
+                first = second;
+                second = val;
+            } else {
+                let new_sum = first + second + val;
+                if new_sum > sum {
+                    count = count + 1;
+                }
+                sum = new_sum;
+                first = second;
+                second = val;
+            }
+        }
+    });
+    count
+}
+
 fn main() -> std::io::Result<()> {
     let file = File::open("input.txt")?;
     let lines = BufReader::new(file).lines();
-    let count = count_increased_depth(lines);
-    println!("Depth increased: {} times", count);
+    //let count = count_increased_depth(lines());
+    //println!("Depth increased: {} times", count);
+    let count = count_increased_sliding_window(lines);
+    println!("Depth sliding window increased: {} times", count);
     Ok(())
 }
