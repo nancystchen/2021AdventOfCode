499d7ee2d3a07efa328ba3bad5ba41ae3fa5f085
nancystchen <nanstchen@gmail.com>
2021-12-06 15:43:00 -0800

    part 1 solution

--- 3/src/main.rs
+++ 3/src/main.rs
@@ -2,22 +2,31 @@ use std::fs::File;
 use std::io::{BufRead, BufReader, Lines};
 
 fn from_binary_str_to_decimal(binary_str: &str) -> i32 {
-    unimplemented!()
+    binary_str
+        .chars()
+        .rev()
+        .enumerate()
+        .fold(0, |acc, (idx, ch)| {
+            let bit: u32 = ch.into();
+            acc + 2_i32.pow(idx as u32) * ((bit - 48)as i32)
+        })
 }
 
 fn calculate_epsilon_rate(gemma_rate: &str) -> String {
-    unimplemented!()
+    String::from_iter(
+        gemma_rate
+            .chars()
+            .map(|ch| if ch == '1' { '0' } else { '1' }),
+    )
 }
 
 fn calculate_gemma_rate(data: Lines<BufReader<File>>) -> String {
-    let mut lines = data
-        .peekable();
+    let mut lines = data.peekable();
     let mut num_one_in_binary_position: Vec<usize> = lines
         .peek()
-        .expect("Cannot find the first line of data").as_ref()
-        .map(|line| {
-            vec![0; line.len()]
-        })
+        .expect("Cannot find the first line of data")
+        .as_ref()
+        .map(|line| vec![0; line.len()])
         .unwrap();
     let mut total_line_count = 0;
     lines.enumerate().for_each(|(idx, line)| {
@@ -29,20 +38,17 @@ fn calculate_gemma_rate(data: Lines<BufReader<File>>) -> String {
         });
         total_line_count = idx;
     });
-    let gemma_rate =
-        num_one_in_binary_position
-            .into_iter()
-            .fold(String::new(), |mut acc, count| {
-                if count > (total_line_count / 2) {
-                    acc.push('1');
-                    acc
-                } else {
-                    acc.push('0');
-                    acc
-                }
-            });
 
-    gemma_rate
+    num_one_in_binary_position
+        .into_iter()
+        .fold(String::new(), |mut acc, count| {
+            if count > (total_line_count / 2) {
+                acc.push('1');
+            } else {
+                acc.push('0');
+            }
+            acc
+        })
 }
 
 fn get_data(file_name: &str) -> Lines<BufReader<File>> {
@@ -54,12 +60,11 @@ fn main() {
     println!("Solving problems...");
     let lines = get_data("input.txt");
     let gemma_rate_str = calculate_gemma_rate(lines);
-    println!("gemma: {}", gemma_rate_str);
-    /*
-    let epsilon_rate_str = calculate_epsilon_rate(gemma_rate);
+    let epsilon_rate_str = calculate_epsilon_rate(&gemma_rate_str);
 
-    let gemma_rate = from_binary_str_to_decimal(gemma_rate_str);
-    let epsilon_rate = from_binary_str_to_decimal(epsilon_rate_str);
+    let gemma_rate = from_binary_str_to_decimal(&gemma_rate_str);
+    println!("epsilon calc");
+    let epsilon_rate = from_binary_str_to_decimal(&epsilon_rate_str);
 
     println!(
         "Gemma rate: {}. Epsilon rate: {}. Power of rates: {}",
@@ -67,5 +72,4 @@ fn main() {
         epsilon_rate,
         gemma_rate * epsilon_rate
     );
-    */
 }
