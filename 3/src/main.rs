5a8b44d3579a80381719de14618a961ffc4b05be
nancystchen <nanstchen@gmail.com>
2021-12-06 20:34:39 -0800

    fix: add weight impl

--- 3/src/main.rs
+++ 3/src/main.rs
@@ -1,5 +1,87 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader, Lines};
+use std::str::Chars;
+
+#[derive(Debug)]
+struct Node {
+    one: Option<Box<Node>>,
+    zero: Option<Box<Node>>,
+    value: String,
+    weight: u32,
+}
+
+impl Node {
+    fn new(value: String) -> Self {
+        Self {
+            one: None,
+            zero: None,
+            value,
+            weight: 0,
+        }
+    }
+
+    
+    fn find(&self, mut path: Chars) -> String {
+        match path.next() {
+            Some('1') => match self.one {
+                None => {
+                    if let Some(ref node) = self.zero {
+                        node.find(path)
+                    } else {
+                        self.value.clone()
+                    }
+                }
+                Some(ref node) => node.find(path),
+            },
+            Some('0') => match self.zero {
+                None => {
+                    if let Some(ref node) = self.one {
+                        node.find(path)
+                    } else {
+                        self.value.clone()
+                    }
+                }
+                Some(ref node) => node.find(path),
+            },
+            _ => self.value.clone(),
+        }
+    }
+    /*
+    fn find(&self) -> String {
+
+    }
+    */
+}
+
+fn add_node_to_bst(root: &mut Node, path: &mut Chars) {
+    match path.next() {
+        Some('1') => {
+            if let Some(ref mut node) = root.one {
+                add_node_to_bst(node, path);
+            } else {
+                let mut new_str = root.value.clone();
+                new_str.push('1');
+                let mut new_node = Node::new(new_str);
+                add_node_to_bst(&mut new_node, path);
+                root.weight += 1 + new_node.weight;
+                root.one = Some(Box::new(new_node));
+            }
+        }
+        Some('0') => {
+            if let Some(ref mut node) = root.zero {
+                add_node_to_bst(node, path);
+            } else {
+                let mut new_str = root.value.clone();
+                new_str.push('0');
+                let mut new_node = Node::new(new_str);
+                add_node_to_bst(&mut new_node, path);
+                root.weight += 1 + new_node.weight; 
+                root.zero = Some(Box::new(new_node));
+            }
+        }
+        _ => {}
+    };
+}
 
 fn from_binary_str_to_decimal(binary_str: &str) -> i32 {
     binary_str
@@ -8,8 +90,22 @@ fn from_binary_str_to_decimal(binary_str: &str) -> i32 {
         .enumerate()
         .fold(0, |acc, (idx, ch)| {
             let bit: u32 = ch.into();
-            acc + 2_i32.pow(idx as u32) * ((bit - 48)as i32)
+            acc + 2_i32.pow(idx as u32) * ((bit - 48) as i32)
         })
+}
+
+fn calculate_oxygen_generator_rating(gemma_rate: &str, bst: &Node) -> i32 {
+    let path = gemma_rate.chars();
+    let o2_str = bst.find(path);
+    println!("{}", o2_str);
+    from_binary_str_to_decimal(&o2_str)
+}
+
+fn calculate_co2_srubber_rating(epsilon_rate: &str, bst: &Node) -> i32 {
+    let path = epsilon_rate.chars();
+    let co2_str = bst.find(path);
+    println!("{}", co2_str);
+    from_binary_str_to_decimal(&co2_str)
 }
 
 fn calculate_epsilon_rate(gemma_rate: &str) -> String {
@@ -20,7 +116,7 @@ fn calculate_epsilon_rate(gemma_rate: &str) -> String {
     )
 }
 
-fn calculate_gemma_rate(data: Lines<BufReader<File>>) -> String {
+fn calculate_gemma_rate(data: Lines<BufReader<File>>, root: &mut Node) -> String {
     let mut lines = data.peekable();
     let mut num_one_in_binary_position: Vec<usize> = lines
         .peek()
@@ -31,21 +127,24 @@ fn calculate_gemma_rate(data: Lines<BufReader<File>>) -> String {
     let mut total_line_count = 0;
     lines.enumerate().for_each(|(idx, line)| {
         let binary_str: String = line.expect("Cannot parse data into binary string");
+        let mut chars_iter = binary_str.chars();
+        add_node_to_bst(root, &mut chars_iter);
+        println!("{}", root.weight);
         binary_str.chars().enumerate().for_each(|(idx, c)| {
             if c == '1' {
                 num_one_in_binary_position[idx] += 1;
             }
         });
-        total_line_count = idx;
+        total_line_count = idx + 1;
     });
 
     num_one_in_binary_position
         .into_iter()
         .fold(String::new(), |mut acc, count| {
-            if count > (total_line_count / 2) {
-                acc.push('1');
-            } else {
+            if count < (total_line_count / 2) {
                 acc.push('0');
+            } else {
+                acc.push('1');
             }
             acc
         })
@@ -58,12 +157,18 @@ fn get_data(file_name: &str) -> Lines<BufReader<File>> {
 
 fn main() {
     println!("Solving problems...");
-    let lines = get_data("input.txt");
-    let gemma_rate_str = calculate_gemma_rate(lines);
+    let lines = get_data("sample_input.txt");
+    let mut bst = Node {
+        one: None,
+        zero: None,
+        value: String::new(),
+        weight: 0,
+    };
+    let gemma_rate_str = calculate_gemma_rate(lines, &mut bst);
+    println!("{}", gemma_rate_str);
     let epsilon_rate_str = calculate_epsilon_rate(&gemma_rate_str);
 
     let gemma_rate = from_binary_str_to_decimal(&gemma_rate_str);
-    println!("epsilon calc");
     let epsilon_rate = from_binary_str_to_decimal(&epsilon_rate_str);
 
     println!(
@@ -72,4 +177,16 @@ fn main() {
         epsilon_rate,
         gemma_rate * epsilon_rate
     );
+
+    let o2_rate = calculate_oxygen_generator_rating(&gemma_rate_str, &bst);
+    let co2_rate = calculate_co2_srubber_rating(&epsilon_rate_str, &bst);
+
+    
+    println!(
+        "o2: {}, co2: {}, life support rating: {}",
+        o2_rate,
+        co2_rate,
+        o2_rate * co2_rate
+    );
+    
 }
