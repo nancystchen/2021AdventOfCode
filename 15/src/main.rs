181a696c9d2751ca8e063768d62fb1ed12db7de7
nancystchen <nanstchen@gmail.com>
2021-12-15 15:54:45 -0800

    feat: improved version using binary heap

--- 15/src/main.rs
+++ 15/src/main.rs
@@ -1,5 +1,7 @@
-// Famous Dijkstra's algorithm question! In very vanilla non-queue fashion...
+// Famous Dijkstra's algorithm question! Improved using BinaryHeap.
 
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
 use std::fs::File;
 use std::io::{BufRead, BufReader, Lines};
 
@@ -8,45 +10,26 @@ fn dijkstras_algo(source: (usize, usize), des: (usize, usize), graph: &[Vec<usiz
     // set dist[source] = 0, other entries usize::MAX.
     let mut dist = vec![vec![usize::MAX; des.1 + 1]; des.0 + 1];
     dist[source.0][source.1] = 0;
-    // create another visited table dis_i * des_j to mark visits.
-    let mut visited = vec![vec![false; des.1 + 1]; des.0 + 1];
+    // create another to_visit queue to host nodes to visit
+    let mut to_visit = BinaryHeap::new();
     // starting from source as current node, for its neighbours:
-    let mut curr = (source.0, source.1);
-    // - if haven't found des node (problem gurantees foundable des)
-    while !visited[des.0][des.1] {
+    to_visit.push(Reverse((0, (source.0, source.1))));
+    // - if haven't visited all neighbours
+    while let Some(Reverse((_, curr))) = to_visit.pop() {
         // - update node neighbours' distances from source if smaller.
         let neighbours = get_neighbours(curr.0, curr.1, graph[0].len(), graph.len());
         neighbours.iter().for_each(|n| {
             let n_dist = dist[curr.0][curr.1] + graph[n.0][n.1];
             if n_dist < dist[n.0][n.1] {
-                dist[n.0][n.1] = n_dist
+                dist[n.0][n.1] = n_dist;
+                // if value is updated, we need to check its neighbours again.
+                // a cycle won't be added because cycle cost more than its subparts.
+                to_visit.push(Reverse((dist[n.0][n.1], (n.0, n.1))));
             }
         });
-
-        // - mark current node as visited
-        visited[curr.0][curr.1] = true;
-        // - find node with the smallest value in dist[] that's unvisited
-        curr = find_unvisited_min(&dist, &visited);
     }
     // return dist[des] value
     dist[des.0][des.1]
-}
-
-// Given a list of nodes and records of visits, find unvisited node that holds smallest value.
-fn find_unvisited_min(dist: &[Vec<usize>], v: &[Vec<bool>]) -> (usize, usize) {
-    let mut min = usize::MAX;
-    let mut min_i = 0;
-    let mut min_j = 0;
-    dist.iter().enumerate().for_each(|(i, row)| {
-        row.iter().enumerate().for_each(|(j, val)| {
-            if *val < min && !v[i][j] {
-                min = *val;
-                min_i = i;
-                min_j = j;
-            };
-        })
-    });
-    (min_i, min_j)
 }
 
 // we can create graph on the fly because of the problem type
@@ -81,7 +64,8 @@ fn parse_data(data: Lines<BufReader<File>>) -> Vec<Vec<usize>> {
     .collect()
 }
 
-// 5x dimension of original graph
+// 5x dimension of given graph
+// TODO: can just use inner logic to calculate value to reduce mem usage.
 fn make_full_graph(graph: &[Vec<usize>]) -> Vec<Vec<usize>> {
     let part_width = graph[0].len();
     let part_height = graph.len();
@@ -105,7 +89,7 @@ fn make_full_graph(graph: &[Vec<usize>]) -> Vec<Vec<usize>> {
 }
 
 fn main() {
-    let file = File::open("input.txt").unwrap();
+    let file = File::open("sample_input.txt").unwrap();
     let data = BufReader::new(file).lines();
     let graph = parse_data(data);
     let shortest_path = dijkstras_algo((0, 0), (graph[0].len() - 1, graph.len() - 1), &graph);
